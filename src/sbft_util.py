da03eca6e660480e917bb9013b25553ff0aa4d65
Aadarsh Venugopal <aadarsh3996@gmail.com>
2021-11-29 09:46:13 -0800

    Added Cryptography utilities

---
+++ src/sbft_util.py
@@ -0,0 +1,18 @@
+import rsa
+
+def generate_rsa_key_pairs():
+    
+    key_pairs = {}
+    public_key, private_key = rsa.newkeys(2048)
+    key_pairs['public_key'] = public_key.save_pkcs1().decode('utf8')
+    key_pairs['private_key'] = private_key.save_pkcs1().decode('utf8')
+    return key_pairs
+
+def encrypt_data(encryption_key, message):
+    
+    return rsa.encrypt(message.encode('utf-8'), rsa.PublicKey.load_pkcs1(encryption_key.encode('utf-8')))
+
+def decrypt_data(decryption_key, message):
+
+    return rsa.decrypt(message, rsa.PrivateKey.load_pkcs1(decryption_key.encode('utf-8'))).decode('utf-8')
+
